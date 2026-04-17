3f1144ea71e270562d7d02e74dd54574c5095662
Mishuko-WoW <mishuko.gamer@gmail.com>
2026-04-17 15:06:42 +0200

    Deploy nueva version botones y opener

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-const isDev = process.argv.includes('dev');
-
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mishuko-wow.github.io',
-	base: isDev ? '/' : '/Guia-BM-Hunter',
+	site: 'https://guiahunterbm.com/',
+	base: '/',
 	image: {
 		domains: ['wow.zamimg.com', 'assets.rpglogs.com'],
 	},
