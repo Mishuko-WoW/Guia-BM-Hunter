ca9ad6f55adf68e6dd4ef8877643a83486945e78
Mishuko-WoW <mishuko.gamer@gmail.com>
2026-05-01 05:54:47 +0200

    Añadir sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://guiahunterbm.com/',
-	base: '/',
-	image: {
-		domains: ['wow.zamimg.com', 'assets.rpglogs.com'],
+  site: 'https://guiahunterbm.com/',
+  base: '/',
+
+  image: {
+      domains: ['wow.zamimg.com', 'assets.rpglogs.com'],
 	},
+
+  integrations: [sitemap()],
 });
